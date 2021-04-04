184cac472d9ef0f18ba9ac1fc03b4a14af14afd9
Sunandhan <79313170+Sunandhan06@users.noreply.github.com>
2021-04-04 19:47:34 +0530

    Update CLOCK.ino

--- CLOCK.ino
+++ CLOCK.ino
@@ -1,1 +1,34 @@
+#include <DS3231.h>
+
+// Init the DS3231 using the hardware interface
+DS3231  rtc(SDA, SCL);
+
+void setup()
+{
+  
+  // Initialize the rtc object
+  rtc.begin();
+  
+  // The following lines can be uncommented to set the date and time
+  //rtc.setDOW(WEDNESDAY);     // Set Day-of-Week to SUNDAY
+  //rtc.setTime(12, 0, 0);     // Set the time to 12:00:00 (24hr format)
+  //rtc.setDate(1, 1, 2014);   // Set the date to January 1st, 2014
+}
+
+void loop()
+{
+  // Send Day-of-Week = rtc.getDOWStr();
+  
+  
+  
+  // Send date = rtc.getDateStr();
+ 
+
+  // Send time = rtc.getTimeStr();
+  
+  // Wait one second before repeating :)
+ // delay (1000);
+}
+
+
 

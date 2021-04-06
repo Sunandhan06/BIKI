15041ef9ed5700f22e79a221b7184ac7d17b3333
Sunandhan <79313170+Sunandhan06@users.noreply.github.com>
2021-04-06 17:00:28 +0530

    Update BIKI_main.ino

--- BIKI_main.ino
+++ BIKI_main.ino
@@ -5,13 +5,76 @@
 
 DS3231  rtc(SDA, SCL);
 
+int button1 = 3;
+ //int button2 = 4;
+
 int hall_effect = 2;
  unsigned int rpm;
   volatile byte revolution;
 
 unsigned long present = 0;
- unsigned long = past = 0;
+ unsigned long past = 0;
   unsigned long = time;
+
+int w1 = 1;
+ int w2 = 1;
+  int w3 = 1;
+
+void window1(int a) {
+
+  int but_val1;
+
+  while(a) {
+
+    but_val1 = digitalRead(button1);
+
+    if(but_val1 == 1) {
+
+
+
+
+    }
+
+    else {
+
+      a = 0;
+
+    }
+
+  }
+
+}
+
+void clock(int b) {
+
+  while(b) {
+    
+    int but_val2 = digitalRead(button1);
+
+    if(but_val2 == 1) {
+
+      // Send Day-of-Week = rtc.getDOWStr();
+  
+      // Send date = rtc.getDateStr();
+ 
+
+      // Send time = rtc.getTimeStr();
+  
+      // Wait one second before repeating :)
+
+      // delay (1000);
+
+    }
+
+    else {
+
+      b = 0;
+
+    }
+
+  }
+
+}
 
 void ISR() {
 
@@ -19,42 +82,46 @@ void ISR() {
 
 }
 
-void rpm() {
+void rpm(int c) {
 
-  delay(1000);
+  while(c) {
 
-  detachInterrupt(digitalPinToInterrupt(hall_effect));
+    int but_val3 = digitalRead(button1);
 
-  past = present;
-   present = millis();
-    time = (present-past)/1000;
+    if(but_val3 == 1) {
 
-  rpm = (revolution/time)*60;
+      delay(1000);
 
-  revolution = 0;
+      detachInterrupt(digitalPinToInterrupt(hall_effect));
 
-  attachInterrupt(digitalPinToInterrupt(hall_effect), ISR, RISING);
+      past = present;
+       present = millis();
+        time = (present-past)/1000;
 
-}
+      rpm = (revolution/time)*60;
 
-void clock() {
+      revolution = 0;
 
-  // Send Day-of-Week = rtc.getDOWStr();
-  
-  // Send date = rtc.getDateStr();
- 
+      attachInterrupt(digitalPinToInterrupt(hall_effect), ISR, RISING);
 
-  // Send time = rtc.getTimeStr();
-  
-  // Wait one second before repeating :)
+    }
 
- // delay (1000);
+    else {
+
+      c = 0;
+
+    }
+
+  }
 
 }
 
 void setup() {
 
   rtc.begin();
+
+  pinMode(button1, INPUT_PULLUP);
+   //pinMode(button2, INPUT_PULLUP);
 
   pinMode(hall_effect, INPUT);
 
@@ -67,5 +134,15 @@ void loop() {
   past = present;
    present = millis();
     time = (present-past)/1000;
+
+  window1(w1);
+   clock(w2);
+    rpm(w3);
+
+  /* w1 = 1;
+      w2 = 1;
+       w3 = 1; */ 
+
+  
 
 }

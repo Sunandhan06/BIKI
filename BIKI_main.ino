bd127e3dd783b2b5af52273e3b719eb0361d2d65
Sunandhan <79313170+Sunandhan06@users.noreply.github.com>
2021-04-07 16:19:29 +0530

    Update BIKI_main.ino

--- BIKI_main.ino
+++ BIKI_main.ino
@@ -19,6 +19,7 @@ unsigned long present = 0;
 int w1 = 1;
  int w2 = 1;
   int w3 = 1;
+   int w4 = 1;
 
 void window1(int a) {
 
@@ -116,6 +117,80 @@ void rpm(int c) {
 
 }
 
+void timer(int d) {
+
+  while(d) {
+
+    //starting display "timer"
+
+    delay(5000);
+
+    but_val4 = digitalRead(button1);
+
+    if(but_val4 == 0) {
+
+      // 10 sec display
+
+      delay(2000)
+
+      but_val5 = digitalRead(button1); 
+
+      if(but_val5 == 0) {
+
+        // 30 sec display
+
+        delay(2000) 
+
+        but_val6 = digitalRead(button1);
+
+        if(but_val6 = 0) {
+
+
+          // 60 sec display
+
+          delay(2000) 
+
+          but_val7 = digitalRead(button1);
+
+          if(but_val7 == 0) {
+
+            d = 0;
+
+          }
+
+          else {
+
+            // 60 sec starts
+            
+          }
+
+        }
+
+        else {
+
+          // 30 sec starts
+
+        }
+      }
+
+      else {
+
+        // 10 sec starts
+
+      }
+    }
+
+    else {
+
+      d = 0;
+
+    }
+
+  }
+
+
+}
+
 void setup() {
 
   rtc.begin();
@@ -138,6 +213,7 @@ void loop() {
   window1(w1);
    clock(w2);
     rpm(w3);
+     timer(w4);
 
   /* w1 = 1;
       w2 = 1;
